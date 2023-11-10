ca608c9a250b46c994abd5c71340dbbb3faaba0f
Ivan Osipov <ivan.osipov@valagroup.com>
2023-11-10 11:03:31 +0200

    Add a lambda to start

--- lamda_fun_start_instance.py
+++ lamda_fun_start_instance.py
@@ -1,8 +1,9 @@
 import boto3
-region = 'eu-north-1'
-instances = ['i-0c9a7348dc231335b']
-ec2 = boto3.client('ec2', region_name=region)
+
+REGION = 'eu-north-1'
+INSTANCES = ['i-0c9a7348dc231335b']
+EC2 = boto3.client('ec2', region_name=REGION)
 
 def lambda_handler(event, context):
-    ec2.start_instances(InstanceIds=instances)
-    print('started your instances: ' + str(instances))
+    EC2.start_instances(InstanceIds=INSTANCES)
+    print(f'started your instances {str(INSTANCES)}')
